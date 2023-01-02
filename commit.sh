06370269d6dea029d3e03b7ae143a93553c4a8e6
meenara14 <meena.rani@sgatechsolutions.com>
2023-01-02 16:42:12 +0000

    WWEMP-49

--- commit.sh
+++ commit.sh
@@ -1,9 +1,3 @@
 # Commit changes
 git commit -a -m "WWEMP-49"
 git push
-
-# Reinstall app
-bench --site $sitename uninstall-app cheque_run --no-backup --yes
-rm -rf ../../apps/cheque_run
-bench get-app https://github.com/tritchot-sga/cheque_run.git
-bench --site $sitename install-app cheque_run
