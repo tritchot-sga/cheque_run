3c3827d3f58b7c2d8b56774e272709df2b2a212f
meenara14 <meena.rani@sgatechsolutions.com>
2023-01-03 21:40:04 +0000

    WWEMP-49

--- cheque_run/cheque_run/doctype/cheque_run/cheque_run.py
+++ cheque_run/cheque_run/doctype/cheque_run/cheque_run.py
@@ -350,7 +350,7 @@ def get_entries(doc):
 			(
 				`tabPurchase Invoice`.due_date <= %(end_date)s OR
 				(
-					`tabPayment Schedule`.discount != 0
+					`tabPayment Schedule`.discount != 0 AND
 					`tabPayment Schedule`.discount_date >= %(start_discount_date)s AND
 					`tabPayment Schedule`.discount_date <= %(end_discount_date)s 
 				)
@@ -498,7 +498,8 @@ def load_get_entries(doc):
 			(
 				`tabPurchase Invoice`.due_date <= %(end_date)s OR
 				(
-					`tabPayment Schedule`.discount != 0
+					`tabPayment Schedule`.discount != 0 AND
+					
 					`tabPayment Schedule`.discount_date >= %(start_discount_date)s AND
 					`tabPayment Schedule`.discount_date <= %(end_discount_date)s 
 				)
